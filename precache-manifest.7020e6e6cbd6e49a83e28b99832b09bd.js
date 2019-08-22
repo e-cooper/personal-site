2d40050de5ce1d0f5ee1ec4774092632548c42d3
Eli Cooper <elicooper18@gmail.com>
2019-08-22 01:29:00 -0500

    Updates

---
+++ precache-manifest.7020e6e6cbd6e49a83e28b99832b09bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a0e0d1df7f57b68f6544c8eef561e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3da97b45dd9005242de4",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5ae7b480ee09ae24bc8e",
+    "url": "/static/js/2.f247b4a6.chunk.js"
+  },
+  {
+    "revision": "3da97b45dd9005242de4",
+    "url": "/static/js/main.f8be5fd8.chunk.js"
+  },
+  {
+    "revision": "9962b327babe52c48101",
+    "url": "/static/js/runtime~main.e9b25b23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
