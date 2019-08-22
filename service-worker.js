a99134611a711c83cc99449b12f053fc53429410
Eli Cooper <elicooper18@gmail.com>
2019-08-22 04:31:00 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7020e6e6cbd6e49a83e28b99832b09bd.js"
+  "/precache-manifest.6b1ae4daf99819601b0f4f048bbc8dc5.js"
 );
 
 self.addEventListener('message', (event) => {
