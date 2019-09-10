e2f1f878fbf6c1efec1864962e15068ea93ad9a9
Eli Cooper <elicooper18@gmail.com>
2019-09-10 01:24:40 -0500

    Add flavor text and icons

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelopeSquare, faHeart, faDraftingCompass, faCode } from '@fortawesome/free-solid-svg-icons'
 import Name from '../components/presentational/Name';
 import Description from '../components/presentational/Description';
+import SocialLink, { TYPES } from '../components/presentational/SocialLink';
 
 import './App.css';
 
-library.add(fab, faEnvelopeSquare);
+library.add(fab, faEnvelopeSquare, faHeart, faDraftingCompass, faCode);
 
 function App() {
   return (
     <div className="App">
       <Name />
       <Description />
+
+      <div className="SocialLinkBox">
+        <SocialLink type={TYPES.LINKEDIN} />
+        <SocialLink type={TYPES.GITHUB} />
+        <SocialLink type={TYPES.EMAIL} />
+      </div>
     </div>
   );
 }
--- src/components/presentational/Description/index.js
+++ src/components/presentational/Description/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import SocialLink, { TYPES } from '../SocialLink';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './index.css';
 
 function Description() {
   return (
     <div className="Description">
-      <div className="SocialLinkBox">
-        <SocialLink type={TYPES.LINKEDIN} />
-        <SocialLink type={TYPES.GITHUB} />
-        <SocialLink type={TYPES.EMAIL} />
+      <p className="DescriptionText">
+        Hi, I'm a Frontend Engineer currently based in Austin, TX.
+      </p>
+
+      <div className="IconRow">
+        <div className="Icon">
+          <FontAwesomeIcon icon='drafting-compass' />
+        </div>
+        <div className="DashedLine" />
+        <div className="Icon">
+          <FontAwesomeIcon icon='heart' />
+        </div>
+        <div className="DashedLine" />
+        <div className="Icon">
+          <FontAwesomeIcon icon='code' />
+        </div>
       </div>
+
+      <p className="DescriptionText">
+        My passion lies at the crossroads of intelligent design and adaptive technology.
+      </p>
     </div>
   );
 }
